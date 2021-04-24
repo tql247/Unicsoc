d4aa34e00106a10c74e78778c64346766390634f
linhYedda <linh.yedda@yedda.ai>
2021-04-24 15:07:15 +0700

    last plan5 + socket

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 require('module-alias/register')
 require('./src/index')
-// require('./src/utils/socket')
+require('./src/utils/socket')
 console.log('Server is running...')
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,11 +1,30 @@
-const { Server } = require("socket.io");
-const io = new Server();
+const httpServer = require('http').createServer();
+const {Server} = require("socket.io");
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5000",
+        methods: ["*"],
+        credentials: true
+    },
+    allowEIO3: true
+});
 
-io.on('connection', function(client){
-    console.log("Hello")
-    client.on('join', function(data){
+io.on('connection', function(client) {
+    console.log('hello')
+    client.on('join', function(data) {
         client.broadcast.emit('newMember', data);
+    });
+    client.on('message', function(data) {
+        // client.emit('thread', data);
+        client.broadcast.emit('thread', data);
+    });
+
+    client.on('disconnect', function(reason) {
+        console.log(reason);
+        client.broadcast.emit('leave');
     });
 });
 
-io.listen(3000);
+httpServer.listen(3000);
+
+console.log('Server is running...')
