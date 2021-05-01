fd544cd5d1268d58fd41981c197a511aed78e42c
linhYedda <linh.yedda@yedda.ai>
2021-05-01 21:57:16 +0700

    add not found exception

--- src/services/get_account_data.js
+++ src/services/get_account_data.js
@@ -2,7 +2,14 @@ const find_account_by_email = require("./accessor/find_account_by_email");
 
 async function get_account_data (email) {
     try {
-        return await find_account_by_email(email)
+        const user = await find_account_by_email(email)
+        if (user)
+            return user
+        const err = new Error();
+        err.message = "Email not exist";
+        err.name = "Page not found"
+
+        throw err
     } catch (e) {
         throw e
     }
